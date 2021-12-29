9b639c8d27da7ddd8e11486d2a334c81f6aefe70
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2021-12-29 14:44:14 +0200

    Add Register/Login logic to authenticate agains Firebase

--- InstChat/Controllers/LoginViewController.swift
+++ InstChat/Controllers/LoginViewController.swift
@@ -6,17 +6,27 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "goToChatAfterLogin", sender: self)
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "goToChatAfterLogin", sender: self)
+                }
+            }
+        }
     }
     
     /*
--- InstChat/Controllers/RegisterViewController.swift
+++ InstChat/Controllers/RegisterViewController.swift
@@ -6,17 +6,29 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func registerButtonPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "goToChatAfterRegister", sender: self)
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "goToChatAfterRegister", sender: self)
+                }
+            }
+        }
+        
+        
     }
     
     /*
